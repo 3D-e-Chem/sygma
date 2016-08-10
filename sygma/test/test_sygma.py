2b1e47d48d3b7d8de701764d05260aa956f94c4c
ridderl <l.ridder@esciencecenter.nl>
2016-08-10 19:44:58 +0000

    Provide parent without coordinates in test_prediction_phenol (#1)

--- sygma/test/test_sygma.py
+++ sygma/test/test_sygma.py
@@ -1,19 +1,20 @@
 import sygma
 from rdkit import Chem
-from rdkit.Chem import AllChem
 
-
-def test_sygma():
+def test_prediction_phenol():
+    """Integration test of sygma module"""
     scenario = sygma.Scenario([
         [sygma.ruleset['phase1'], 1],
         [sygma.ruleset['phase2'], 1]
     ])
 
     parent = Chem.MolFromSmiles("c1ccccc1O")
-    Chem.AllChem.Compute2DCoords(parent)
+
     metabolites_network = scenario.run(parent)
     metabolites_network.calc_scores()
     metabolites_network.add_coordinates()
 
     metabolite_list = metabolites_network.to_list()
     assert len(metabolite_list) == 12
+    assert metabolite_list[0]['SyGMa_score'] == 1
+    assert metabolite_list[1]['SyGMa_path'] == 'O-glucuronidation_(aromatic_hydroxyl); \n'
