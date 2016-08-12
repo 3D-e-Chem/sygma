ef781dfb9500d8f8fac448de1a6a9ec02a7f6d5f
ridderl <l.ridder@esciencecenter.nl>
2016-08-12 14:35:00 +0000

    Split test of treenode methods

--- sygma/test/test_units.py
+++ sygma/test/test_units.py
@@ -3,8 +3,18 @@ from rdkit import Chem, Geometry
 from rdkit.Chem import AllChem
 
 
-def test_treenode():
-    """Test functionality of TreeNode class"""
+def test_treenode_init():
+    """Test init of TreeNode class"""
+
+    # generate phenol molecule
+    mol = Chem.MolFromSmiles('c1ccccc1O')
+    node = sygma.TreeNode(mol)
+
+    # test if inchikey is generated correctly
+    assert node.ikey == "ISWSIDIOOBJBQZ"
+
+def test_treenode_gen_coords():
+    """Test gen_coords method of TreeNode class"""
 
     # generate phenol molecule with coordinates
     mol = Chem.MolFromSmiles('c1ccccc1O')
@@ -17,9 +27,6 @@ def test_treenode():
     node = sygma.TreeNode(mol)
     # generate coordinates for atoms without atom positions (i.e. all coordinates are zero)
     node.gen_coords()
-
-    # test if inchikey is generated correctly
-    assert node.ikey == "ISWSIDIOOBJBQZ"
 
     # test if existing coordinates are unchanged in gen_coords method
     assert conf.GetAtomPosition(2).x == pos2x
