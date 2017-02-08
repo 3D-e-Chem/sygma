bd14ef349df31da3e12cf9d4ce818a0c451f0c52
Lars Ridder <l.ridder@esciencecenter.nl>
2017-02-08 13:34:48 +0100

    Set sphinx-argparse dependency

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
+    'sphinxarg.ext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
